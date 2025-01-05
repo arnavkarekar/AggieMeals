7cf5b9bcee26c85088e4a7355ad233d66ce93206
Arnav Karekar <arnavkarekar72034@gmail.com>
2025-01-05 15:37:17 -0600

    added a login page

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -7,22 +7,72 @@ export default function StatsScreen() {
   return (
     <>
       <HomeHeader />
-      <View style = {styles.box}>
-        <Text style = {styles.textColor}>
-          Hello, World!
+      <View style = {styles.parent}>
+        <Text style = {styles.heading}>
+          Login
         </Text>
+        <View>
+          <Text style = {styles.inputTitle}>
+            Username
+          </Text>
+          <TextInput style = {styles.input} placeholder="Type your username"></TextInput>
+          <Text style = {styles.inputTitle}>
+            Password
+          </Text>
+          <TextInput style = {styles.input} placeholder="Type your password"></TextInput>
+        </View>
+        <TouchableOpacity style = {styles.button}>
+          LOGIN
+        </TouchableOpacity>
+        <Text>
+          or
+        </Text>
+        <Image>
+        </Image>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  box: {
-    height: 50, 
-    width: 50, 
-    backgroundColor: 'green'
+  parent: {
+    backgroundColor: '#ececec',
+    flex: 1,
+    alignItems: 'center'
   },
   textColor: {
-    color: 'white'
+    color: 'black'
+  },
+  heading: {
+    color: "black",
+    fontSize: 30,
+    fontWeight: 700,
+    marginTop: 50
+  },
+  input: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    borderWidth: .1,
+    borderColor: "#a6a6a6",
+    width: 300,
+    height: 40,
+    color: "gray",
+    paddingLeft: 10
+  },
+  inputTitle: {
+    margin: 5,
+    marginTop: 15
+  },
+  button: {
+    width: 300,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: "#840000",
+    fontFamily: "System",
+    color: "white",
+    fontWeight: 600,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20
   }
 });
